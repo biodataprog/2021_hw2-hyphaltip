a8795c640ea1c536149980e282c30114d0256371
Jason Stajich <jasonstajich.phd@gmail.com>
2021-11-03 15:56:19 -0700

    solved problem 1

--- squared_cubed.py
+++ squared_cubed.py
@@ -1,10 +1,37 @@
 #!/usr/bin/env python3
 
 # you can change this for experimentation
-MaxNum = 30
+MaxNum = 100
 
 # this will print a header
 print("\t".join(["N","Squared","Cubed"]))
+
+print(n,n**2,n**3,sep="\t")
+
+for n in range(MaxNum):
+    print("\t".join([str(n),str(n**2),str(n**3)]))
+
+print("\n")
+#print("\t".join(["N","Squared","Cubed"]))
+#for n in range(MaxNum):
+#    print("\t".join([str(n),"%e"%(n**2),"%g"%(n**3)]))
+
+print("\n")
+print("{}\t{}\t{}".format("N","Squared","Cubed"))
+for n in range(MaxNum):
+    print("{}\t{}\t{}".format(n,n**2,n**3,"notes"))
+#    print("%d\t%d\t%g".format(n,n**2,n**3))
+#    print("\t".join([str(n),"%e"%(n**2),"%g"%(n**3)]))
+import csv
+import sys
+csvwriter = csv.writer(sys.stdout, delimiter=',',quoting=csv.QUOTE_MINIMAL)
+
+csvwriter.writerow(["N","Squared","Cubed"])
+for n in range(MaxNum):
+    row = [n,n**2,n**3,"extra notes"]
+    if n > 3:
+        row.append("more notes")
+    csvwriter.writerow(row)
 
 # write your code below you can use
 # range(MaxNum)
